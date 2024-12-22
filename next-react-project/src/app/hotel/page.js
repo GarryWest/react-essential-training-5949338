52386fde3838de4d8f9b876b65def748dcd2ea34
Garry West <garrywest@msn.com>
2024-12-22 17:35:04 +0000

    Moving on to 06_03b...

--- next-react-project/src/app/hotel/page.js
+++ next-react-project/src/app/hotel/page.js
@@ -1,8 +1,30 @@
-export default function Page(){
+async function getData() {
+  const res = await fetch (
+    "https://snowtooth-hotel-api.fly.dev"
+  );
+  return res.json();
+}
+
+function HotelBlock({ name, capacity }) {
+  return (
+    <div>
+      <h2>
+        {name}
+      </h2>
+      {capacity}
+    </div>
+  )
+}
+
+export default async function Page(){
+  const data = await getData();
   return (
     <main>
       <div>
           <h1>Hotel Details</h1>
+          <div>{data.map((hotel) => (
+            <HotelBlock name = {hotel.name} capacity = {hotel.capacity} />
+          ))}</div>
       </div>
     </main>
   );
