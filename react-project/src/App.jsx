b382267dc6bc0d943214c5455dc373e16c7b957e
Garry West <garrywest@msn.com>
2024-12-22 16:07:09 +0000

    More state stuff

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/prop-types */
+// import { useState } from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
+import chef from "./images/chef.jpg";
 
 function Header({ name, year }) {
   return (
@@ -20,23 +24,56 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
-    <ul>
-      {dishes.map((dish) => (
-        <li key={dish.id} style={{ listStyleType: "none" }}>
-          {dish.title}
-        </li>
-      ))}
-    </ul>
+    <>
+    <div>
+      {/* <button onClick={() => onStatus(!openStatus)}> */}
+      <button onClick={() => onStatus()}>
+        I want to be {openStatus ? "closed" : "open"}
+      </button>
+      <h2>Welcome to this beautiful restaurant! {openStatus ? "Open" : "Closed"}</h2>
+    </div>
+    <main>
+      <img 
+        src={chef} 
+        //src="https://github.com/garrywest.png"
+        height={200} 
+        alt="A photo of a smiling chef owner" 
+      />
+      <ul>
+        {dishes.map((dish) => (
+          <li key={dish.id} style={{ listStyleType: "none" }}>
+            {dish.title}
+          </li>
+        ))}
+      </ul>
+    </main>
+    </>
   );
 }
 
 function App() {
+  //const [status, setStatus] = useState(true);
+
+  const [status, toggle] = useReducer(
+    (status) => !status,
+    true
+  );
+  
+  useEffect (() => {
+    console.log(`The restaurant is ${status ? "open" : "closed"}`)
+  }, [status]);
+
   return (
     <div>
-      <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <h1>The restaurant is currently {status ? "open" : "closed"}</h1>
+      {/* <button onClick={() => setStatus(!status)}> */}
+      <button onClick={() => toggle()}>
+        {status ? "Close" : "Open"} Restaurant
+      </button>
+      <Header name="Garry" year={new Date().getFullYear()} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={toggle} />
     </div>
   );
 }
